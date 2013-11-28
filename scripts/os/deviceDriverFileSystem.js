cc13877bc7c7c237c0e78a7e97e47fffab47b94d
Mike Hunter <randomemail149@gmail.com>
2013-11-27 20:17:36 -0500

    File System implementaion

--- scripts/os/deviceDriverFileSystem.js
+++ scripts/os/deviceDriverFileSystem.js
@@ -16,4 +16,39 @@ function DeviceDriverFileSystem() {
 
 function FSDriverEntry() {
 	this.status = "loaded";
+	
+	createFS();
+}
+
+function createFS() {
+	
+	localStorage.clear();
+	
+	var key = "";
+	var value = "";
+	
+	var fileData = [];
+	
+	//Fills the data area with '~' initially
+	for(var fileDataSize = 1; fileDataSize < 59; fileDataSize++) {
+		fileData[0]= "\"~";
+		fileData[fileDataSize] = "~";
+		fileData[59]= "~\"";		
+	}
+	
+	for(var track = 0; track <= 3; track++) {
+		for(var sector = 0; sector <= 7; sector++) {
+			for(var block = 0; block <= 7; block++) {
+				
+				key = [track, sector, block];
+				value = ["0", "~", "~", "~", fileData];
+				
+				localStorage[key] = value;
+				
+			}
+		}
+	}
+	//Place Master Boot Record in location 000
+	var mbr = ["1", "M", "B", "R", fileData];
+	localStorage["0,0,0"] = mbr;
 }
